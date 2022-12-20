0a6acd5750ed0e4e0cde710ccadc2b226bf39ace
tung <00957001@email.ntou.gmail.com>
2022-12-20 20:00:30 +0800

    Initial Commit

---
+++ soccer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  soccer
+//
+//  Created by User05 on 2022/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ soccer/soccerApp.swift
@@ -0,0 +1,17 @@
+//
+//  soccerApp.swift
+//  soccer
+//
+//  Created by User05 on 2022/12/20.
+//
+
+import SwiftUI
+
+@main
+struct soccerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
